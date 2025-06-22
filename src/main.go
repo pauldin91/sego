9072a94b639469302ce5ae5fcf1e977b9f90f831
pauldin91@gmail.com <pauldin91@gmail.com>
2025-06-22 10:36:02 +0300

    setup main window

--- src/main.go
+++ src/main.go
@@ -6,13 +6,25 @@ import (
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
+var topWindow fyne.Window
+var preferenceCurrentTutorial = []byte(time.Now().UTC().String())
+
 func main() {
-	app := app.NewWithID(hex.EncodeToString([]byte(time.Now().UTC().String())))
+	app := app.NewWithID(hex.EncodeToString(preferenceCurrentTutorial))
 	w := app.NewWindow("Hakuna Mattata")
 	w.SetFixedSize(true)
+	// message := widget.NewLabel("Welcome")
+	// button := widget.NewButton("Update", func() {
+	// 	formatted := time.Now().Format("Time: 03:04:05")
+	// 	message.SetText(formatted)
+	// })
+
+	// w.SetContent(container.NewVBox(message, button))
 	w.Resize(fyne.NewSize(640, 460))
 	w.ShowAndRun()
-	w.Close()
+	//w.Close()
 }
