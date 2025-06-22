aa8f6197104cdc74f98a6da7c2df12aede56f192
pauldin91@gmail.com <pauldin91@gmail.com>
2025-06-22 18:01:05 +0300

    basic image loading

--- src/main.go
+++ src/main.go
@@ -1,30 +1,76 @@
 package main
 
 import (
-	"encoding/hex"
+	"image"
+	"io"
 	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/storage"
 	"fyne.io/fyne/v2/widget"
 )
 
-var topWindow fyne.Window
-var preferenceCurrentTutorial = []byte(time.Now().UTC().String())
+var (
+	w            fyne.Window
+	imgContainer *canvas.Image
+	imgLabel     *widget.Label
+)
 
 func main() {
-	app := app.NewWithID(hex.EncodeToString(preferenceCurrentTutorial))
-	w := app.NewWindow("Hakuna Mattata")
-	w.SetFixedSize(true)
-	message := widget.NewLabel("Welcome")
-	button := widget.NewButton("Update", func() {
-		formatted := time.Now().Format("Time: 03:04:05")
-		message.SetText(formatted)
-	})
+	a := app.NewWithID("image.viewer." + time.Now().String())
+	w = a.NewWindow("Hakuna Matata")
+	w.Resize(fyne.NewSize(640, 480))
 
-	w.SetContent(container.NewVBox(message, button))
-	w.Resize(fyne.NewSize(640, 460))
+	// Initial blank image
+	blank := image.NewRGBA(image.Rect(0, 0, 640, 480))
+
+	imgContainer = canvas.NewImageFromImage(blank)
+	imgContainer.FillMode = canvas.ImageFillContain
+	imgContainer.SetMinSize(fyne.NewSize(640, 400))
+
+	imgLabel = widget.NewLabel("No image loaded")
+	openBtn := getDialogButton(w)
+
+	content := container.NewVBox(
+		openBtn,
+		imgLabel,
+		imgContainer,
+	)
+	w.SetContent(content)
 	w.ShowAndRun()
-	w.Close()
+}
+
+func getDialogButton(win fyne.Window) *widget.Button {
+	return widget.NewButton("Open Image", func() {
+		fd := dialog.NewFileOpen(selectImage, win)
+
+		fd.SetFilter(storage.NewExtensionFileFilter([]string{".png", ".jpg", ".jpeg"}))
+		fd.Show()
+	})
+}
+
+func selectImage(reader fyne.URIReadCloser, err error) {
+	if err != nil {
+		dialog.ShowError(err, w)
+		return
+	}
+	if reader == nil {
+		return // user cancelled
+	}
+
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		dialog.ShowError(err, w)
+		return
+	}
+	res := fyne.NewStaticResource(reader.URI().Name(), data)
+
+	imgContainer.Resource = res
+	imgContainer.Refresh()
+	imgLabel.SetText("Loaded: " + reader.URI().Name())
+
 }
